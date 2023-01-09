0c748da6ca75932845288845b7be9658b8ba7d00
AleesandroDiNardo <alexdna003@gmal.com>
2023-01-09 16:55:55 +0100

    punto1 completato

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>
+  <?php
+    $title = "First PHP exercise";
+    echo $title;
+  ?>
+</h1>
+
+<p>
+  <?php
+    $lorem = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptas soluta quas, necessitatibus animi dolorem minus ea ab quaerat aliquam dolorum veniam impedit rem alias veritatis nemo vel? Velit, inventore!";
+    echo $lorem;
+  ?>
+</p>
+
+<div>
+  <?php
+    echo "La lunghezza del verso è " . strlen($lorem) . " caratteri";
+  ?>
+</div>
+</body>
+</html>
